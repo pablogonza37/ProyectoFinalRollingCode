4f6063965e13b2e30250b7abb45f01754f658598
Pablo Gonzalez <pab.gonzalez3798@gmail.com>
2024-03-28 21:57:17 -0300

    se creo el componente Menu

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
-import './App.css'
+import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import Inicio from './components/pages/Inicio'
+import Inicio from "./components/pages/Inicio";
+import Menu from "./components/commons/Menu";
+import { useState } from "react";
 
 function App() {
- 
 
   return (
     <>
-    <Inicio></Inicio>
+      <Menu></Menu>
+      <Inicio></Inicio>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
