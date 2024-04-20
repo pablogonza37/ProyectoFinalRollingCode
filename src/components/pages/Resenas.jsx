141d1c0c1a50a1a2dcc27e8612f7e7c5c9b180d1
Pablo Gonzalez <pab.gonzalez3798@gmail.com>
2024-04-20 18:13:44 -0300

    se agrego mensaje cuando no hay reseñas

--- src/components/pages/Resenas.jsx
+++ src/components/pages/Resenas.jsx
@@ -107,6 +107,18 @@ const Resenas = ({ usuarioLogueado }) => {
 
   return (
     <section>
+      <div className="text-center my-4">
+      {resenias.length === 0 ? (
+        <>
+        <h3>No hay reseñas disponibles.</h3>
+       {usuarioLogueado && (
+          <Button onClick={handleShow} variant="dark">
+            <i className="bi bi-plus-circle"></i>
+          </Button>
+        )}
+        </>
+      ) : null}
+    </div>
       <Carousel
         className="carousel-background my-4 p-5 d-flex justify-content-center position-relative shadow"
         wrap={true}
